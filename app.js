b8465f2c19a1fe0ec6272b299a53cc03a6f9c84e
Biparker <bparker873@gmail.com>
2023-11-17 11:09:28 -0500

    Update app.js database connect

--- app.js
+++ app.js
@@ -27,7 +27,12 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect("mongodb://localhost:27017/userDB");
+
+
+mongoose.connect('mongodb://localhost:27017/userDB', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...', err));
+
 
 const userSchema = new mongoose.Schema({
     email: String,
